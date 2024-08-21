94ebb30f73d3f945bb6b1db234fb35706e1d9a3f
Koki <kokimalinda@gmail.com>
2024-08-21 17:00:58 +0300

    fifth commit

--- leap_year.py
+++ leap_year.py
@@ -3,6 +3,8 @@ def leap_year():
 
     if year % 4 == 0:
         print(f"{year} is a leap year.")
+    elif year < 1:
+        print("Error: Invalid Input!")
     else:
         print(f"{year} is not a leap year.")
 
